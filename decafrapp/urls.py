9f777e1756ffa915cdf58f93d41017bb703eac4f
zanebliss <zanebliss@icloud.com>
2020-09-11 10:19:51 -0500

    added register route in decafrapp.urls.py

--- decafrapp/urls.py
+++ decafrapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import home
+from .views import register
 
 app_name = 'decafrapp'
 
 urlpatterns = [
     path('', home, name='home'),
     path('accounts/', include('django.contrib.auth.urls'))
+    # path('register/', register, name='register')
 ]
