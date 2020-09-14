8f0b4714337ec4691eeacc5cbb2a7191d2883d7e
zanebliss <zanebliss@icloud.com>
2020-09-14 09:53:36 -0500

    added login and register links and home conditional rendering

--- decafrapp/views/home.py
+++ decafrapp/views/home.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
-    if request.method == 'GET':
-        return render(request, 'home.html')
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            return render(request, 'home.html') 
+    else:
+        return redirect('accounts/login')
