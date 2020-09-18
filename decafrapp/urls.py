35dab599e6a9d805d6b79a3653d0e4adb872d37e
zanebliss <zanebliss@icloud.com>
2020-09-18 13:55:23 -0500

    added editing a drink

--- decafrapp/urls.py
+++ decafrapp/urls.py
@@ -8,10 +8,11 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
     path('logout', views.logout_view, name='logout'),
+    path('newentry', views.entry_form, name='newentry'),
     path('entries', views.entry_list, name='entries'),
     path('entries/<int:pk>', views.entry_detail, name='entry'),
+    path('entries/<int:pk>/form', views.entry_edit_form, name='entryedit'),
+    path('newdrink', views.drink_form, name='newdrink'),
     path('drinks', views.drink_list, name='drinks'),
-    path('dirnks/<int:pk>', views.drink_detail, name='drink'),
-    path('newentry', views.entry_form, name='newentry'),
-    path('newdrink', views.drink_form, name='newdrink')
+    path('drinks/<int:pk>', views.drink_detail, name='drink'),
 ]
